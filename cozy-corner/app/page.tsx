0a48a53003a130b89775157598237f62b4e4e59d
tarunmeena6846 <tarunmeena6846@gmail.com>
2024-11-04 22:00:37 +0530

    gallary, map, review changes

--- cozy-corner/app/page.tsx
+++ cozy-corner/app/page.tsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 import { Index } from "./components/Nav";
 import { Hero } from "./components/Hero/page";
 import { About } from "./components/AboutUs/Index";
+import { Service } from "./components/Services/Service";
+import { Review } from "./components/Review/Review";
+import MapEmbed from "./components/Map/Map";
+import { Gallery } from "./components/Gallery/Gallery";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
       <About />
+      <Service />
+      <Review />
+      <Gallery />
+      <MapEmbed />
     </div>
   );
 }
